b65481e37ad8cf97cd818603373cf48efecc7b3b
Yakumo-Sahashi <diego.bollas@hotmail.com>
2021-06-05 23:39:09 -0500

    URL AMIGABLES

--- index.php
+++ index.php
@@ -4,10 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'app/config.php';?>
     <?php require_once 'app/dependencias.php';?>
     <title>AMATERASU</title>
 </head>
 <body>
-    <h1>Hola</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <a class="btn btn-primary btn-block" href="home">Inicio</a>
+            </div>
+            <div class="col-2">
+                <a class="btn btn-primary btn-block" href="login">Iniciar sesion</a>
+            </div>
+            <div class="col-2">
+                <a class="btn btn-primary btn-block" href="registro">Registro</a>
+            </div>
+        </div>
+    </div>
+    <?php
+        if(isset($_GET['view'])){
+            $url = explode("/", $_GET['view']);
+
+            switch($url[0]){
+                case 'home': 
+                    require_once 'view/home.php';
+                    break;
+                case 'login': 
+                    require_once 'view/login/login.php';
+                    break;
+
+                default:
+                    require_once 'view/404.php';
+                    break;
+
+            }           
+
+        }else{
+            require_once 'view/home.php';
+        }
+    ?>
 </body>
 </html>
