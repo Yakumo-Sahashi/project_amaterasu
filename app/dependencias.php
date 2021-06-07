c29dff10d03b4652d0d62893963d69e9f58fecae
esecharly <crls38frack@hotmail.com>
2021-06-07 00:08:32 -0500

    se crearon las cards de usuario y la de la tabla

--- app/dependencias.php
+++ app/dependencias.php
@@ -1,8 +1,9 @@
-<link rel="stylesheet" href="css/b4/bootstrap.css">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="<?=SERVIDOR?>css/b4/bootstrap.css">
+<link rel="stylesheet" href="<?=SERVIDOR?>css/font_awesome/all.css">
+<link rel="stylesheet" href="<?=SERVIDOR?>css/main.css">
 
-<script src="js/font_awesome/all.js"></script>
-<script src="js/swal/swal.js"></script>
-<script src="js/jquery/jquery.js"></script>
-<script src="js/poper/popper.js"></script>
-<script src="js/b4/bootstrap.js"></script>
+<script src="<?=SERVIDOR?>js/font_awesome/all.js"></script>
+<script src="<?=SERVIDOR?>js/swal/swal.js"></script>
+<script src="<?=SERVIDOR?>js/jquery/jquery.js"></script>
+<script src="<?=SERVIDOR?>js/poper/popper.js"></script>
+<script src="<?=SERVIDOR?>js/b4/bootstrap.js"></script>
--- index.php
+++ index.php
@@ -4,11 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'app/config.php';?>
     <?php require_once 'app/dependencias.php';?>
     <title>AMATERASU</title>
 </head>
 <body>
-    <h1>Hola</h1>
+    <?php require_once 'view/navbar.php';?>
     
+    <?php
+        if(isset($_GET['view'])){
+            $url = explode("/", $_GET['view']);
+
+            switch($url[0]){
+                case 'home': 
+                    require_once 'view/home.php';
+                    break;
+                case 'login': 
+                    require_once 'view/login.php';
+                    break;
+                case 'materias': 
+                    require_once 'view/materias.php';
+                    break;
+
+                default:
+                    require_once 'view/404.php';
+                    break;
+
+            }           
+
+        }else{
+            require_once 'view/home.php';
+        }
+    ?>
 </body>
 </html>
