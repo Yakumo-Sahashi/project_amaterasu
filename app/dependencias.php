093036ef8df53e41e1c83e4a6767860b6211c1e1
Yakumo-Sahashi <diego.bollas@hotmail.com>
2021-06-07 19:09:43 -0500

    favico y descripcion

--- app/dependencias.php
+++ app/dependencias.php
@@ -1,8 +1,10 @@
-<link rel="stylesheet" href="<?=SERVIDOR?>css/b4/bootstrap.css">
-<link rel="stylesheet" href="<?=SERVIDOR?>css/main.css">
 
-<script src="<?=SERVIDOR?>js/font_awesome/all.js"></script>
-<script src="<?=SERVIDOR?>js/swal/swal.js"></script>
-<script src="<?=SERVIDOR?>js/jquery/jquery.js"></script>
-<script src="<?=SERVIDOR?>js/poper/popper.js"></script>
-<script src="<?=SERVIDOR?>js/b4/bootstrap.js"></script>
+    <link rel="icon" type="image/ico" href="<?=SERVIDOR;?>img/itma2.png">
+    <link rel="stylesheet" href="<?=SERVIDOR?>css/b4/bootstrap.css">
+    <link rel="stylesheet" href="<?=SERVIDOR?>css/main.css">
+
+    <script src="<?=SERVIDOR?>js/font_awesome/all.js"></script>
+    <script src="<?=SERVIDOR?>js/swal/swal.js"></script>
+    <script src="<?=SERVIDOR?>js/jquery/jquery.js"></script>
+    <script src="<?=SERVIDOR?>js/poper/popper.js"></script>
+    <script src="<?=SERVIDOR?>js/b4/bootstrap.js"></script>
