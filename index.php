a0f67f1759ebf5ed7ef92d8cb7884f3346fc10d7
Rodrigo <rodryest34@hotmail.com>
2021-06-09 11:33:21 -0500

    cambios que puede hacer a secas

--- index.php
+++ index.php
@@ -4,11 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'app/config.php';?>
     <?php require_once 'app/dependencias.php';?>
     <title>AMATERASU</title>
 </head>
 <body>
-    <h1>Hola</h1>
-    <h2>Zombie ipsum reversus ab viral inferno, nam rick grimes malum</h2>
+    <?php require_once 'view/nav.php'?>
+    <?php
+        if(isset($_GET['view'])){
+            $url = explode("/", $_GET['view']);
+
+            switch($url[0]){
+                case 'home': 
+                    require_once 'view/home.php';
+                    break;
+                case 'docente': 
+                    require_once 'view/docente/panel.php';
+                    break;
+                case 'alumno': 
+                    require_once 'view/alumno/panel.php';
+                    break;
+                case 'login': 
+                    require_once 'view/login/login.php';
+                    break;
+
+                default:
+                    require_once 'view/404.php';
+                    break;
+
+            }           
+
+        }else{
+            require_once 'view/login/login.php';
+        }
+    ?>
 </body>
 </html>
---
+++ view/home.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-5">
+            <h2>estoy en el home</h2>
+        </div>
+    </div>
+</div>
