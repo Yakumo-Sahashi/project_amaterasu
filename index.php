7eb46eaebc2fc18fdb025068e579324735004d60
Yakumo-Sahashi <diego.bollas@hotmail.com>
2021-06-16 22:57:44 -0500

    v1

--- index.php
+++ index.php
@@ -19,26 +19,45 @@
                 case 'home': 
                     require_once 'view/home.php';
                     break;
+            //--------------admin----------------------------------
                 case 'administrador': 
                     require_once 'view/admin/panel.php';
                     break;
+                case 'subirDocentes': 
+                    require_once 'view/admin/docentes.php';
+                    break;
+                case 'tablaAlumnos': 
+                    require_once 'view/admin/tablaAlumno.php';
+                    break;
+            //-------------docente---------------------------------
                 case 'docente': 
                     require_once 'view/docente/panel.php';
                     break;
+                case 'asignarCalificaciones': 
+                    require_once 'view/docente/calificaciones.php';
+                    break;
+                case 'tablaUsuario': 
+                    require_once 'view/docente/tablaUsuario.php';
+                    break;
+            //------------alumno----------------------------------
                 case 'alumno': 
                     require_once 'view/alumno/panel.php';
+                    break;
+                case 'examenesAlumnos': 
+                    require_once 'view/alumno/examenes.php';
                     break;
                 case 'archivos': 
                     require_once 'view/alumno/archivos.php';
                     break;
-                case 'calendario': 
-                    require_once 'view/calendario.php';
-                    break;
                 case 'grafica': 
                     require_once 'view/alumno/grafica.php';
                     break;
+            //-------------general------------------------------
                 case 'login': 
                     require_once 'view/login/login.php';
+                    break;
+                case 'calendario': 
+                    require_once 'view/calendario.php';
                     break;
 
                 default:
