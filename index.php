85e3b120c71e3e72d7b240a1407e0ab5b70e2e33
Yakumo-Sahashi <diego.bollas@hotmail.com>
2021-06-18 09:15:26 -0500

    error 404

--- index.php
+++ index.php
@@ -17,74 +17,78 @@
         if(isset($_GET['view'])){
             $url = explode("/", $_GET['view']);
 
-            switch($url[0]){
-                case 'home': 
-                    require_once 'view/home.php';
-                    break;
-            //----------------admin-------------------------
-                case 'admin': 
-                    require_once 'view/admin/panel.php';
-                    break;    
-                case 'docentes': 
-                    require_once 'view/admin/tablaDocentes.php';
-                    break;
-                case 'materias': 
-                    require_once 'view/admin/materias.php';
-                    break;
-                case 'usuariosActivos': 
-                    require_once 'view/admin/usuariosActivos.php';
-                    break;
-                case 'registroAlumnos': 
-                    require_once 'view/admin/tablaAlumno.php';
-                    break;
-            //----------------docente-------------------------
-                case 'docente': 
-                    require_once 'view/docente/panel.php';
-                    break;
-                case 'calificaciones': 
-                    require_once 'view/docente/calificaciones.php';
-                    break;
-                case 'materiasDocente': 
-                    require_once 'view/docente/materias.php';
-                    break;
-                case 'asignacionExamen': 
-                    require_once 'view/docente/examenes.php';
-                    break;
-                case 'alumnos': 
-                    require_once 'view/docente/tablaAlumno.php';
-                    break;
-            //----------------alumno-------------------------
-                case 'alumno': 
-                    require_once 'view/alumno/panel.php';
-                    break;
-                case 'consultaCalificaciones': 
-                    require_once 'view/alumno/calificaciones.php';
-                    break;   
-                case 'horario': 
-                    require_once 'view/alumno/materias.php';
-                    break;   
-                case 'examenes': 
-                    require_once 'view/alumno/examenes.php';
-                    break;            
-            //----------------general-------------------------
-                case 'login': 
-                    require_once 'view/login/login.php';
-                    break; 
-                case 'semestre': 
-                    require_once 'view/calendario.php';
-                    break;
-                case 'grafica': 
-                    require_once 'view/grafica.php';
-                    break;
-                case 'archivos': 
-                    require_once 'view/archivos.php';
-                    break;     
-                //----------------404-------------------------             
-                default:
-                    require_once 'view/404.php';
-                    break;
+            if(count($url)<2)
 
-            }           
+                switch($url[0]){
+                    case 'home': 
+                        require_once 'view/home.php';
+                        break;
+                //----------------admin-------------------------
+                    case 'admin': 
+                        require_once 'view/admin/panel.php';
+                        break;    
+                    case 'docentes': 
+                        require_once 'view/admin/tablaDocentes.php';
+                        break;
+                    case 'materias': 
+                        require_once 'view/admin/materias.php';
+                        break;
+                    case 'usuariosActivos': 
+                        require_once 'view/admin/usuariosActivos.php';
+                        break;
+                    case 'registroAlumnos': 
+                        require_once 'view/admin/tablaAlumno.php';
+                        break;
+                //----------------docente-------------------------
+                    case 'docente': 
+                        require_once 'view/docente/panel.php';
+                        break;
+                    case 'calificaciones': 
+                        require_once 'view/docente/calificaciones.php';
+                        break;
+                    case 'materiasDocente': 
+                        require_once 'view/docente/materias.php';
+                        break;
+                    case 'asignacionExamen': 
+                        require_once 'view/docente/examenes.php';
+                        break;
+                    case 'alumnos': 
+                        require_once 'view/docente/tablaAlumno.php';
+                        break;
+                //----------------alumno-------------------------
+                    case 'alumno': 
+                        require_once 'view/alumno/panel.php';
+                        break;
+                    case 'consultaCalificaciones': 
+                        require_once 'view/alumno/calificaciones.php';
+                        break;   
+                    case 'horario': 
+                        require_once 'view/alumno/materias.php';
+                        break;   
+                    case 'examenes': 
+                        require_once 'view/alumno/examenes.php';
+                        break;            
+                //----------------general-------------------------
+                    case 'login': 
+                        require_once 'view/login/login.php';
+                        break; 
+                    case 'semestre': 
+                        require_once 'view/calendario.php';
+                        break;
+                    case 'grafica': 
+                        require_once 'view/grafica.php';
+                        break;
+                    case 'archivos': 
+                        require_once 'view/archivos.php';
+                        break;     
+                    //----------------404-------------------------             
+                    default:
+                        require_once 'view/404.php';
+                        break;
+
+                }  
+            else
+                header("location:".SERVIDOR."404");        
 
         }else{
             require_once 'view/login/login.php';
